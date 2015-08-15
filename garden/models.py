0b9f94c4695640c32d53ec1f308450a3cdfe3709
Carol Willing <carolcode@willingconsulting.com>
2015-08-14 18:18:07 -0700

    Added garden model

--- garden/models.py
+++ garden/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Garden(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
